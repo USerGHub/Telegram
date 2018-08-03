485cf3e18c25dd13dba85f5321c21689cf8dc99a
USerGHub <42064403+USerGHub@users.noreply.github.com>
2018-08-04 00:43:46 +0700

    Delete Isotopes_bot.py

--- Isotopes_bot.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot('606971001:AAGYAt9B8RskqCjczuZ9q5D0UtLLl_QX8jI')
-
-def handle_text(message):
-    if message.text == 'Прив':
-        bot.send_message(message.from_user.id, 'Здраствуй')
-    
-bot.polling(none_stop=True, interval = 1)
